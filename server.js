6b4a569149b1a5a9bcbefca4cd6505ca0916e0f4
ankit <ankit.chachada@gmail.com>
2017-05-16 14:42:49 -0700

    node code

---
+++ server.js
@@ -0,0 +1,57 @@
+var express        = require('express');  
+var app            = express();
+var http = require('http');
+var socketIo = require('socket.io');
+
+// set our port
+var port = process.env.PORT || 3000;
+
+// set the static files location 
+app.use(express.static(__dirname + '/public'));
+
+require('./app/routes')(app); // configure our routes
+
+// startup our app at http://localhost:3000
+//app.listen(port);
+var server =  http.createServer(app);
+
+var io = socketIo.listen(server);
+server.listen(port);
+
+
+// array of all lines drawn
+var line_history = [];
+
+// event-handler for new incoming connections
+io.on('connection', function (socket) {
+
+   // first send the history to the new client
+   for (var i in line_history) {
+      console.log(i);
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history
+      line_history.push(data.line);
+
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+
+   socket.on('erase', function () {
+      line_history = [];
+      io.emit('erase', { line: [] });
+   });
+
+   socket.on('color', function (data) {
+      io.emit('color', { col : data.col });
+   });
+
+});
+
+
+
+// expose app
+exports = module.exports = app;
